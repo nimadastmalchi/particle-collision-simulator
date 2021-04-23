2cc148d3018ac7b794dd8d12e4f24d74f988e337
BuildTools <unconfigured@null.spigotmc.org>
2021-04-23 01:31:24 -0700

    4/23/2021

--- simulator.py
+++ simulator.py
@@ -22,7 +22,7 @@ class Vector:
 
     def distance(self, other) -> float:
         if isinstance(other, self.__class__):
-            return abs(abs(self) - abs(other))
+            return abs(self - other) # BUG not correct
         else:
             raise TypeError(f"unsupported operand type(s) for distance: {self.__class__} and {type(other)}")
 
@@ -80,19 +80,45 @@ def reflect(v : Vector, n : Vector) -> Vector:
     return v - n * v.dot(n) * 2
 
 class Rocket:
-    VEL_LOSS_ON_BOUNCE_RATIO = 0.75 # velocity becomes 75% at each bounce
-    def __init__(self, init_pos: Vector, init_vel : Vector, WIDTH : int, HEIGHT : int):
+    VEL_LOSS_ON_BOUNCE_RATIO = 0.85 # velocity becomes 75% at each bounce
+    def __init__(self, init_pos: Vector, init_vel : Vector, radius : float, SCREEN_WIDTH : int, SCREEN_HEIGHT : int, color : tuple):
         self.pos = init_pos
         self.vel = init_vel
+        self.radius = radius
         self.t = 0
         self.fuel = False
-        self.WIDTH, self.HEIGHT = WIDTH, HEIGHT
+        self.SCREEN_WIDTH, self.SCREEN_HEIGHT = SCREEN_WIDTH, SCREEN_HEIGHT
+        self.color = color
+
+    def get_color(self) -> tuple:
+        return self.color
+
+    def get_loc_tuple(self) -> (float, float):
+        return (self.pos.getx(), self.pos.gety())
+
+    def get_loc_vector(self) -> Vector:
+        return self.pos
+
+    def get_vel(self) -> Vector:
+        return self.vel
+
+    def get_rad(self) -> float:
+        return self.radius
+
+    def get_mass(self) -> float:
+        return self.radius**2 # m = p * A = p * pi * r**2 = r**2, assume p = 1/pi
+
+    def add_time(self, amount : float) -> None:
+        self.t += amount
 
     def add_vel(self, delta_vel : Vector) -> None:
         self.vel = self.vel + delta_vel
 
     def update_vel(self, vel : Vector) -> None:
         self.vel = vel
+
+    def apply_friction(self) -> None:
+        self.vel = self.vel * 0.99
 
     def update_loc(self, dt : float) -> None:
         self.t += dt
@@ -101,17 +127,17 @@ class Rocket:
         # account for bounce on edge of screen
         newx, newy = self.pos.getx(), self.pos.gety()
         n = None
-        if newx <= 0:
-            newx = 0
+        if newx - self.radius <= 0:
+            newx = self.radius
             n = Vector(1, 0)
-        elif newx >= self.WIDTH:
-            newx = self.WIDTH
+        elif newx + self.radius >= self.SCREEN_WIDTH:
+            newx = self.SCREEN_WIDTH - self.radius
             n = Vector(-1, 0)
-        if newy <= 0:
-            newy = 0
+        if newy - self.radius <= 0:
+            newy = self.radius
             n = Vector(0, 1)
-        elif newy >= self.HEIGHT:
-            newy = self.HEIGHT
+        elif newy + self.radius >= self.SCREEN_HEIGHT:
+            newy = self.SCREEN_HEIGHT - self.radius
             n = Vector(0, -1)
 
         if n is not None:
@@ -119,27 +145,71 @@ class Rocket:
 
         self.pos.setvals((newx, newy))
  
-    def get_loc_tuple(self) -> (float, float):
-        return (self.pos.getx(), self.pos.gety())
-
-    def get_loc_vector(self) -> Vector:
-        return self.pos
-
     def __str__(self):
         return f"Rocket[pos={self.pos}, vel={self.vel}, t={self.t}]"
 
-def collision_check(this : Rocket, rockets : [], radius : int):
+# @depreciated
+def collision_check2(this : Rocket, rockets : [], dt : float) -> None:
     for other in rockets:
         if this is other:
             continue
-        this_loc, other_loc = this.get_loc_vector(), other.get_loc_vector()
-        if this.get_loc_vector().distance(other.get_loc_vector()) < radius: #TODO: change to distancesquared to optimize
-            this_dir = this_loc / abs(this_loc)
-            other_dir = other_loc / abs(other_loc)
-            new_this = other_dir * abs(this_loc)
-            new_other = this_dir * abs(other_loc)
-            this.update_vel(new_this)
-            other.update_vel(new_other)
- 
+        grav_check(this, other, dt)
+        x10 = this.get_loc_vector()
+        x20 = other.get_loc_vector()
+        if x10.distance(x20) <= this.get_rad() + other.get_rad() and abs(x20 - x10) != 0: #TODO: change to distancesquared to optimize
+            print("Collision")
+            v10 = this.get_vel()
+            v20 = other.get_vel()
+            m1 = this.get_mass()
+            m2 = other.get_mass()
+            v1n = v10 - ( (x10 - x20) * ((v10 - v20).dot(x10 - x20) / abs(x10 - x20)**2) ) * ( 2*m2 / (m1 + m2) )
+            v2n = v20 - ( (x20 - x10) * ((v20 - v10).dot(x20 - x10) / abs(x20 - x10)**2) ) * ( 2*m1 / (m1 + m2) )
+
+            this.update_vel(v1n)
+            other.update_vel(v2n)
+
+            # update locations so same collision is not detected between same objects again
+            for _ in range(10):
+                this.update_loc(dt)
+                other.update_loc(dt)
+
+def collision_check(r1 : Rocket, r2 : Rocket, dt : float) -> bool:
+    if r1 is r2:
+        return
+
+    x10 = r1.get_loc_vector()
+    x20 = r2.get_loc_vector()
+    if x10.distance(x20) <= r1.get_rad() + r2.get_rad(): #TODO: change to distancesquared to optimize
+        print("Collision")
+        v10 = r1.get_vel()
+        v20 = r2.get_vel()
+        m1 = r1.get_mass()
+        m2 = r2.get_mass()
+        v1n = v10 - ( (x10 - x20) * ((v10 - v20).dot(x10 - x20) / abs(x10 - x20)**2) ) * ( 2*m2 / (m1 + m2) )
+        v2n = v20 - ( (x20 - x10) * ((v20 - v10).dot(x20 - x10) / abs(x20 - x10)**2) ) * ( 2*m1 / (m1 + m2) )
+
+        r1.update_vel(v1n)
+        r2.update_vel(v2n)
+
+        # update locations so same collision is not detected between same objects again
+        for _ in range(2):
+            r1.update_loc(dt)
+            r2.update_loc(dt)
+    
+        return True
+
+    return False
+
+def grav_check(r1 : Rocket, r2 : Rocket, dt : float) -> None:
+    if r1 is r2:
+        return
+    dir_vec = r2.get_loc_vector() - r1.get_loc_vector() # points from r1 to r2
+    dir_vec = dir_vec / abs(dir_vec)
+    d = abs(r1.get_loc_vector() - r2.get_loc_vector())
+    delta_v = dir_vec * r2.get_mass() * dt / d**2
+    r1.add_vel(delta_v * 1000)
+    r1.update_loc(dt)
+
 if __name__ == "__main__":
-    print(Vector(1, 1).distance(Vector(2, 2)))
+    v = Vector(1, 2)
+    print(v / abs(v))
--- viewer.py
+++ viewer.py
@@ -7,25 +7,34 @@ import random
 # constants
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+PINK = (204, 0, 102)
+ORANGE = (255, 128, 0)
+GREY = (64, 64, 64)
+ZERESHKI = (102, 0, 0)
+CYAN = (0, 255, 255)
+
+BALL_COLORS = [BLACK, GREEN, RED, BLUE, PINK, ORANGE, GREY, ZERESHKI, CYAN]
 
 WIDTH = 1000
-HEIGHT = 700
+HEIGHT = 800
 
-GRAV_DELTA_V = 0.1
+GRAV_DELTA_V = 1
 
 REFRESH_RATE = 60
 
-ROCKET_RADIUS = 10
+RADIUS_MIN = 10
+RADIUS_MAX = 60
 
-INIT_NUM_ROCKETS = 1000
+INIT_NUM_ROCKETS = 3
 
-dt = 1
+dt = 1 / 60
 
 def draw_rocket(screen, r : sim.Rocket):
     loc_tuple = r.get_loc_tuple()
-    pygame.draw.circle(screen, BLACK, tuple(map(round, loc_tuple)), ROCKET_RADIUS, 0)
+    pygame.draw.circle(screen, r.get_color(), tuple(map(round, loc_tuple)), r.get_rad(), 0)
     # random colors
     #pygame.draw.circle(screen, tuple(random.randint(0, 255) for _ in range(3)), tuple(map(round, loc_tuple)), ROCKET_RADIUS, 0)
 
@@ -40,11 +49,12 @@ if __name__ == "__main__":
     # used to control refresh rate
     clock = pygame.time.Clock()
 
-    rockets = [sim.Rocket(sim.Vector(random.randint(0, WIDTH), random.randint(0, HEIGHT)), sim.Vector(0, 0), WIDTH, HEIGHT) for _ in range(INIT_NUM_ROCKETS)]
+    rockets = [sim.Rocket(sim.Vector(random.randint(0, WIDTH), random.randint(0, HEIGHT)), sim.Vector(0, 0), random.randint(RADIUS_MIN, RADIUS_MAX+1), WIDTH, HEIGHT, BALL_COLORS[random.randrange(len(BALL_COLORS))]) for _ in range(INIT_NUM_ROCKETS)]
 
     mouse_pos = None
 
     fuel = False
+    attract = None
 
     # init font for putting down text
     pygame.font.init()
@@ -57,14 +67,20 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 carryOn = False
             elif event.type == pygame.MOUSEBUTTONDOWN: # once mouse is down, fueling starts (rocket accelerates toward mouse)
-                fuel = True
+                mouse_state = pygame.mouse.get_pressed() # tuple
+                fuel = not mouse_state[1] # fuel up if either left or right button is pressed (not middle mouse button)
+                attract = mouse_state[0] # attract if left button is pressed
             elif event.type == pygame.MOUSEBUTTONUP: # once mouse is back up, fueling stops
                 fuel = False
             elif event.type == pygame.KEYDOWN:
-                print("Adding rocket")
-                rocket = sim.Rocket(sim.Vector(WIDTH / 2, HEIGHT / 2), sim.Vector(0, 0), WIDTH, HEIGHT)
+                print("Adding rocket...")
+                mouse_pos = pygame.mouse.get_pos()
+                init_loc = sim.Vector(WIDTH / 2, HEIGHT / 2)
+                init_vel = sim.Vector(0, 0).setvals(mouse_pos) - init_loc
+                init_vel = init_vel
+                rocket = sim.Rocket(init_loc, init_vel, random.randint(RADIUS_MIN, RADIUS_MAX+1), WIDTH, HEIGHT, BALL_COLORS[random.randrange(len(BALL_COLORS))])
                 rockets.append(rocket)
- 
+
         # background color
         screen.fill(WHITE)
 
@@ -73,14 +89,23 @@ if __name__ == "__main__":
         screen.blit(textsurface, (0, 0))
 
         for rocket in rockets:
-            #sim.collision_check(rocket, rockets, ROCKET_RADIUS)
+            for other_rocket in rockets:
+                if rocket is other_rocket:
+                    continue
 
-            rocket.add_vel(sim.Vector(0, GRAV_DELTA_V)) # gravity
+                if not sim.collision_check(rocket, other_rocket, dt): # apply collision
+                    sim.grav_check(rocket, other_rocket, dt) # apply gravity if these two objects did not collide so that the objects are not forced into each other
+                
+
+            #rocket.add_vel(sim.Vector(0, GRAV_DELTA_V)) # gravity
+            #rocket.apply_friction()
 
             if fuel:
+                # calculate vector from mouse_pos to rocket:
                 mouse_pos = pygame.mouse.get_pos()
                 delta_vel = sim.Vector().setvals(mouse_pos) - rocket.get_loc_vector()
-                delta_vel = delta_vel / abs(delta_vel) * 10
+                delta_vel = delta_vel / abs(delta_vel) * 100
+                delta_vel = delta_vel * (1 if attract else -1)
                 rocket.add_vel(delta_vel) # adding velocity; hence, accelerating (dv/dt =/= 0)
 
             rocket.update_loc(dt)
